88998ed10bea8f75263c83e6dc38569875f62fac
Iliya Peter Maimolo <39605807+molowaves@users.noreply.github.com>
2021-06-08 23:11:46 +0100

    Update settings.py

--- Journalism/settings.py
+++ Journalism/settings.py
@@ -8,9 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^ypk9z9al%6d87^+^!i2y$*vthk_8zopg-7wq!lo0mt%4eqd8&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
